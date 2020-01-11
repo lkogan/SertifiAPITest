9d320834a54ecf1a1d89cf6f7afadae40d90f708
lkogan <k_leo@hotmail.com>
2020-01-11 14:50:02 -0600

    Add project files.

---
+++ SertifiAPITest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SertifiAPITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
